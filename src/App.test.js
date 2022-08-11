16eee1e3782ad29e4b53fe736df3fb10ef822937
Gonzo <gvidal.industrial@gmail.com>
2022-08-11 00:03:47 -0500

    Add a form with three input fields for email, password and password confirmation with TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { click } from '@testing-library/user-event/dist/click';
 import App from './App';
 
-test('renders learn react link', () => {
+test('inputs should be initially empty', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInputElement = screen.getByRole('textbox');
+  const passwordInputElement = screen.getByLabelText('Password');
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  expect(emailInputElement.value).toBe('');
+  expect(passwordInputElement.value).toBe('');
+  expect(confirmPasswordInputElement.value).toBe('');
+});
+
+test('should be able to type an email', () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole('textbox', {
+    name: /Email address/i,
+  });
+  userEvent.type(emailInputElement, 'gonzalovidal.dev@gmail.com');
+  expect(emailInputElement.value).toBe('gonzalovidal.dev@gmail.com');
+});
+
+test('should be able to type a password', () => {
+  render(<App />);
+  const passwordInputElement = screen.getByLabelText('Password');
+  userEvent.type(passwordInputElement, 'pass');
+  expect(passwordInputElement.value).toBe('pass');
+});
+
+test('should be able to type a password confirmation', () => {
+  render(<App />);
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  userEvent.type(confirmPasswordInputElement, 'pass');
+  expect(confirmPasswordInputElement.value).toBe('pass');
+});
+
+test('should show email error message on invalid email', () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole('textbox', {
+    name: /Email address/i,
+  });
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+  const errorEmailMessage = screen.getByText('Email input is invalid');
+  expect(errorEmailMessage).not.toBeInTheDocument();
+
+  userEvent.type(emailInputElement, 'gonzalovidal.devgmail.com');
+  userEvent.click(submitButton);
+  expect(errorEmailMessage).toBeInTheDocument();
 });
